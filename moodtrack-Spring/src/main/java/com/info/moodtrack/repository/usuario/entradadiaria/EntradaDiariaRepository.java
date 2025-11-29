73a5c2a9f4859a24f1117e2504d4080b7e97de72
iktomy87 <tomyrodriguez1939@gmail.com>
2025-11-29 15:03:25 -0300

    Tarea Moodtrack

---
+++ moodtrack-Spring/src/main/java/com/info/moodtrack/repository/usuario/entradadiaria/EntradaDiariaRepository.java
@@ -0,0 +1,7 @@
+package com.info.moodtrack.repository.usuario.entradadiaria;
+
+import com.info.moodtrack.model.EntradaDiaria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntradaDiariaRepository extends JpaRepository<EntradaDiaria, Long> {
+}
